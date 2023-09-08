ad9be018fb99103682a69e7d0f747c315bd22187
Nathan Byers <natebyers@gmail.com>
2023-09-08 21:56:07 +0000

    duh

--- macros/create_pr_database.sql
+++ macros/create_pr_database.sql
@@ -1,10 +1,10 @@
 {% macro create_pr_database() -%}
 
-    {% set create_database_sql="create database if not exists " +  {{ env_var("DBT_CLOUD_PR_ID") }} %}
+    {% set create_database_sql="create database if not exists " +  env_var("DBT_CLOUD_PR_ID")  %}
     {% do run_query(create_database_sql) %}
     {{ log("database created", info=True) }}
 
-    {% set grant_database_privileges_sql="grant all on database " + {{ env_var("DBT_CLOUD_PR_ID") }} + " to role accountadmin" %}
+    {% set grant_database_privileges_sql="grant all on database " +  env_var("DBT_CLOUD_PR_ID")  + " to role accountadmin" %}
     {% do run_query(grant_database_privileges_sql) %}
     {{ log("database grants given", info=True) }}
 
