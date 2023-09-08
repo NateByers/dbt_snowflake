12b66975a73d13c49a3243b4d00b3086e37fcf3c
Nathan Byers <natebyers@gmail.com>
2023-09-08 21:06:35 +0000

    dbt cloud environment variable

--- macros/create_pr_database.sql
+++ macros/create_pr_database.sql
@@ -2,15 +2,23 @@
 
     {% if pr_database is none%}
 
-        pr_database={{ env_var('DBT_CLOUD_PR_ID') }}
+        pr_database={{ env_var("DBT_CLOUD_PR_ID") }}
+
+    {% elif env_var("DBT_DATABASE") == "PR" %}
+
+        pr_database={{ env_var("DBT_CLOUD_PR_ID") }}
+
+    {% else %}
+
+        pr_database="PR"
 
     {% endif %}
 
-    {% set create_database_sql='create database if not exists ' +  pr_database %}
+    {% set create_database_sql="create database if not exists " +  pr_database %}
     {% do run_query(create_database_sql) %}
     {{ log("database created", info=True) }}
 
-    {% set grant_database_privileges_sql='grant all on database ' + pr_database + ' to role accountadmin'%}
+    {% set grant_database_privileges_sql="grant all on database " + pr_database + " to role accountadmin" %}
     {% do run_query(grant_database_privileges_sql) %}
     {{ log("database grants given", info=True) }}
 
